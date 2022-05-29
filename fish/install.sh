8a99f758b060be5bb1a18172e9c5fddf3b979b26
jordynski <jordynski@tascent.com>
2022-05-29 14:29:40 +0200

    added .gitignore

--- fish/install.sh
+++ fish/install.sh
@@ -5,7 +5,7 @@ function install_fish() {
   ln -s /home/${SUDO_USER}/.debian-config/fish /home/${SUDO_USER}/.config/fish
   su ${SUDO_USER}
   echo /usr/bin/fish | sudo tee -a /etc/shells
-  chsh -s /usr//bin/fish
+  chsh -s /usr/bin/fish
 }
 
 install_fish
