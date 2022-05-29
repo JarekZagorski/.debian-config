005a1ca0411a9ee98d95417e4ac641b348d7aa67
jordynski <jordynski@tascent.com>
2022-05-29 11:41:24 +0200

    tmux and terminal fixed

--- xfce4/terminal/install.sh
+++ xfce4/terminal/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # TEST=${HOME}
-DIR=${PWD}/..
+DIR=/home/${SUDO_USER}
 
 function install_terminal() {
   apt install xfce4-terminal -y
   ln -s ${DIR}/.debian-config/xfce/ ${DIR}/.config/
+  update-alternatives --set x-terminal-emulator /usr/bin/xfce4-terminal.wrapper
 }
 
 install_terminal
