486216a54804b95e4806d9c2f9c7e2b4481c89af
Jakub Ordyński <me.kubolski@gmail.com>
2022-06-08 10:35:24 +0200

    fixed vim and fish install scripts

--- fish/install.sh
+++ fish/install.sh
@@ -2,6 +2,7 @@
 
 function install_fish() {
   apt install fish -y
+  rm -rf /home/${SUDO_USER}/.config/fish
   ln -s /home/${SUDO_USER}/.debian-config/fish /home/${SUDO_USER}/.config/fish
   su ${SUDO_USER}
   echo /usr/bin/fish | sudo tee -a /etc/shells
--- vim/install.sh
+++ vim/install.sh
@@ -2,6 +2,7 @@
 
 function install_vim() {
   apt install vim -y
+  rm -rf /home/${SUDO_USER}/.vim
   ln -s /home/${SUDO_USER}/.debian-config/vim /home/${SUDO_USER}/.vim
 }
 
