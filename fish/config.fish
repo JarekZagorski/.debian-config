21566ae007f74f2a9c6392d96e4e46a5082324d4
Jarek Zagórski <me.kubolski@gmail.com>
2022-10-24 16:48:31 +0200

    General improvements

--- fish/config.fish
+++ fish/config.fish
@@ -4,3 +4,9 @@ if status is-interactive
 end
 
 set LESS -r
+
+# opam configuration
+source /home/jakub/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
+
+# go config
+set -a PATH /usr/local/go/bin
