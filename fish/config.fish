bcd7ec0fc435bbdb917b7f68d8c3387df67b812b
Jarek Zagórski <me.kubolski@gmail.com>
2022-11-23 15:55:46 +0100

    icons and themes

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
   set fish_greeting  
-  set --path --append PATH /usr/local/go/bin
 end
 
 function ..
@@ -12,6 +11,3 @@ set LESS -r
 
 # opam configuration
 source /home/jakub/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
-
-# go config
-set -a PATH /usr/local/go/bin
