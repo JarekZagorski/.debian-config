7ea16d039bae2e0ceeb6a65ddd6dc043e27e8360
Jarek Zagórski <me.kubolski@gmail.com>
2024-01-13 23:59:57 +0100

    alacritty - increased font size, nvim - QOL fixes

--- nvim/lua/custom/set.lua
+++ nvim/lua/custom/set.lua
@@ -5,12 +5,16 @@ o.tabstop = sw
 o.softtabstop = sw
 o.shiftwidth = sw
 o.expandtab = true
+
 o.nu = true
 o.rnu = true
 
 o.smartindent = true
 
 o.wrap = false
+
+o.hlsearch = false
+o.incsearch = true
 
 o.hlsearch = false
 o.incsearch = true
@@ -23,4 +27,7 @@ o.isfname:append('@-@')
 
 o.updatetime = 50
 
+o.colorcolumn = "120"
+
 vim.g.mapleader = ' '
+
